703db9096a58e3c1577e399b8b57af1b279257ad
Diego Mera <diegomera@usp.br>
2020-10-22 20:59:22 -0300

    Front end do CRUD de clientes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Alugamer.Controllers
 		{
 			return View();
 		}
+		public IActionResult CRUDClients()
+		{
+			return View();
+		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
