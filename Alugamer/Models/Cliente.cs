b11bdfb9ffd3826bf3fadb36b9e07a70bb547a49
Voull <gabrielviana57@gmail.com>
2020-11-08 22:50:29 -0300

    Branch do CRUDClientes Reparo do projeto da branch original

---
+++ Alugamer/Models/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Alugamer.Models
+{
+	public class Cliente
+	{
+		public int Id {get; set;}
+		public string Nome {get; set;}
+		public string Email {get; set;}
+		public string Telefone {get; set;}
+		public string Endereco {get; set;}
+		public DateTime DataNascimento {get; set;}
+		public string Sexo {get; set;}
+		public string Cpf {get; set;}
+
+		public List<String> validar()
+		{
+			return new List<string>();
+		}
+	}
+}
