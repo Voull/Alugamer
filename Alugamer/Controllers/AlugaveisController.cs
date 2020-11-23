96494845edecbe4128e7299edf6aaaa40914b568
Diego Mera <diegomera@usp.br>
2020-11-23 00:38:10 -0300

    Corrigindo o erro da outra branch

---
+++ Alugamer/Controllers/AlugaveisController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Alugamer.Database;
+using Alugamer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Alugamer.Controllers
+{
+	public class AlugaveisController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
