c2eb24b397a6db1bf79f5c610e5596fe10ad81e1
Voull <gabrielviana57@gmail.com>
2021-01-11 23:54:55 -0300

    Criação da View de Login

---
+++ Alugamer/wwwroot/js/login.js
@@ -0,0 +1,57 @@
+﻿$(document).ready(function () {
+    //Para Texto Geral
+    setInputFilter(document.getElementById("campoUsuario"), function (value) {
+        return !/["']/.test(value);
+    });
+
+    setInputFilter(document.getElementById("campoSenha"), function (value) {
+        return !/["']/.test(value);
+    });
+};
+
+$('#modalLogin').on('shown.bs.modal', function (e) {
+    Login();
+});
+
+$('#formLogin').on('submit', function () {
+    $("#modalLogin").modal({
+        backdrop: "static"
+    });
+    return false;
+});
+
+function Login() {
+    var url = "/Login/Login";
+
+    var usuario = $("#campoUsuario").val();
+    var senha = $("#campoSenha").val();
+
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: { Usuario: usuario, Senha: senha, Captcha: captcha },
+
+        success: function (data) {
+            data = JSON.parse(data);
+
+            if (!data.Item1) {
+                document.getElementById("modalErroMensagem").innerHTML = data.Item2;
+
+                $("#modalLogin").modal('hide');
+                $("#modalErro").modal();
+                grecaptcha.reset();
+            }
+            else {
+                document.location.href = data.Item2;
+            }
+
+        },
+
+        error: function () {
+            document.getElementById("modalErroMensagem").innerHTML = "Erro na Validação do Login. Tente Novamente mais tarde.";
+            $("#modalLogin").modal('hide');
+            $("#modalErro").modal();
+            grecaptcha.reset();
+        }
+    });
+}
