974a8d17a4095abb78eb5e8a2954acb29defb3f3
Diego Mera <diegomera@usp.br>
2021-01-17 12:06:02 -0300

    Tela de relatórios

---
+++ Alugamer/Controllers/RelatorioController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Alugamer.CRUD;
+using Alugamer.Database;
+using Alugamer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Alugamer.Controllers
+{
+	public class RelatorioController : Controller
+	{
+		public IActionResult Index()
+		{
+			CRUDClientes crudClientes = new CRUDClientes();
+
+			List<Cliente> listaClientes = crudClientes.Lista();
+
+			ViewBag.listaClientes = listaClientes;
+
+			return View();
+		}
+
+
+		[HttpPost]
+		public IActionResult BuscaTodos([FromBody] Aluguel aluguel)
+		{
+			CRUDRelatorio crudRelatorio = new CRUDRelatorio();
+
+			List<Aluguel> listaAluguel = crudRelatorio.buscaTodos(aluguel.DataInicial, aluguel.DataFinal);
+
+			return Ok(JsonConvert.SerializeObject(listaAluguel));
+		}
+
+		[HttpPost]
+		public IActionResult BuscaCliente([FromBody] DateTime DataInicial, [FromBody] DateTime DataFinal, [FromBody] int Id)
+		{
+			CRUDRelatorio crudRelatorio = new CRUDRelatorio();
+
+			List<Aluguel> listaAluguel = crudRelatorio.BuscaCliente(DataInicial, DataFinal, Id);
+
+			return Ok(JsonConvert.SerializeObject(listaAluguel));
+		}
+
+	}
+}
---
+++ Alugamer/wwwroot/js/Relatorio.js
@@ -0,0 +1,80 @@
+﻿$(document).ready(function () {
+    $('#tbCategoria').DataTable({
+        dom: 'Bfrtip',
+        select: true,
+        language: {
+            url: "//cdn.datatables.net/plug-ins/1.10.22/i18n/Portuguese-Brasil.json"
+        }
+    });
+
+});
+
+
+function buscar() {
+
+    var id = Number($('#cliente').val());
+
+    if (id == 0) {
+        alert("Favor escolher um Cliente ou a opção Todos");
+    }
+    else {
+        if (id == -1) {
+            // busca todos no período fornecido
+
+            var dataIni = $("#dataIni").val();
+            var dataFim = $("#dataFim").val();
+
+            let aluguel = {};
+
+            aluguel.DataInicial = dataIni;
+            aluguel.DataFinal = dataFim;
+
+            aluguel = JSON.stringify(aluguel);
+
+
+            $.ajax({
+                url: "Relatorio/BuscaTodos",
+                data: aluguel,
+                type: "application/json",
+                success: function (data) { successBuscaTodos(data); },
+                error: function (data) { failBuscaTodos(data); }
+            });
+
+        }
+        else {
+            // busca os alugueis de um usuário especifico no período fornecido
+
+            var dataIni = $("#dataIni").val();
+            var dataFim = $("#dataFim").val();
+
+                $.ajax({
+                    url: "Relatorio/BuscaCliente",
+                    data: { DataInicial: dataIni, DataFim: dataFim , Id: id},
+                    type: "application/json",
+                    success: function (data) { successBuscaCliente(data); },
+                    error: function (data) { failBuscaCliente(data); }
+                });
+
+        }
+    }
+}
+
+function successBuscaTodos(data) {
+
+
+    var x = 1;
+}
+
+function failBuscaTodos(data) {
+    parsed = data.responseJSON;
+    alert(parsed);
+}
+
+function successBuscaCliente(data) {
+    var cx = 1;
+}
+
+function failBuscaCliente(data) {
+    parsed = data.responseJSON;
+    alert(parsed);
+}
